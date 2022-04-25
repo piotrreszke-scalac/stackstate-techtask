0e78c91fbc6e89967947f2c995ec0c36de23c064
Piotr Reszke <piotr.reszke@ligadigital.com>
2022-04-25 15:37:45 +0200

    reorder commands

--- sock-shop/apply.sh
+++ sock-shop/apply.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 DIR="$(dirname "$BASH_SOURCE")"
-curl -o ${DIR}/complete-demo.yaml https://raw.githubusercontent.com/microservices-demo/microservices-demo/5193b1eeb310a2abdefc1a3786d0666ca7d4324b/deploy/kubernetes/complete-demo.yaml
-
 export KUBECONFIG=${DIR}/../infra/kubeconfig
+
+curl -o ${DIR}/complete-demo.yaml https://raw.githubusercontent.com/microservices-demo/microservices-demo/5193b1eeb310a2abdefc1a3786d0666ca7d4324b/deploy/kubernetes/complete-demo.yaml
 
 kubectl apply -f ${DIR}/complete-demo.yaml
